de54cc5abc06740ebf4c01b82d74efa63843477f
Burn-rebel <alexeikorchkov@gmail.com>
2015-12-25 17:56:39 +0200

    Gallery_scroll

---
+++ Gallery_scroll.py
@@ -0,0 +1,4 @@
+for i in range(22):
+                        self.driver.swipe(0, 632, 0, 237, 2500)
+                        self.driver.find_element_by_xpath("//android.view.View[1]/android.view.View[1]")
+                        time.sleep(1)
